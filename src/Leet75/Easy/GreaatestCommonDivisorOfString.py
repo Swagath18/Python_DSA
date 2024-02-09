ec2ca38d92cf5a70d8c2e8473a4c604b89d0df37
Swagath Babu <115916408+Swagath18@users.noreply.github.com>
2024-02-09 00:15:33 -0800

    Update leet75

--- src/Leet75/Easy/GreaatestCommonDivisorOfString.py
+++ src/Leet75/Easy/GreaatestCommonDivisorOfString.py
@@ -1,1 +1,23 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+    
+        def GCD(a,b):
+            while b:
+                a, b = b, a%b 
+            return a
 
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        greatest_length = GCD(len(str1), len(str2))
+
+        largest_string = str1[:greatest_length]
+
+        return largest_string
+
+# Test cases
+problem = Solution()
+str1 = "ABAB"
+str2 = "ABABAB"
+output = problem.gcdOfStrings(str1, str2)
+print(output)  # Expected Output: "AB"
